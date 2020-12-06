9c2986eb79dfe3f3f914cd279e87a2cc80a54e67
asus <asu9421@gmail.com>
2020-12-06 17:44:48 +0530

    included in mocha

--- array_02.js
+++ array_02.js
@@ -1,15 +1,30 @@
-numbers = ['One', 'Two','Three','Four','Five','Six']
-numbers[0] = '1'
-console.log(numbers)
-// number shift
-numbers.shift()
-console.log("After Shift: " + numbers)
-console.log(numbers.shift())
-console.log("After Shift: " + numbers)
-numbers.unshift('hello')
-console.log("After unshift: " + numbers)
+describe('Array Concepts', () => {
+    it('asif', () => {
+        function isEven(element) {
+            return element % 2 == 0;
+        }
+        console.log(isEven(12));
+    });
+    it('my second test', () => {
+        var isEven = (element) => {
+            return element % 2 == 0;
+        }
 
-numbers = ['One', 'Two','Three','Four','Five','Six']
-console.log(numbers)
-numbers.pop()
-console.log("After Pop: " + numbers)
+        var result = [2, 4, 9].every(isEven)
+        console.log(result)
+    });
+
+    it('test2', () => {
+        var result = [2, 4, 9].every((e) => {
+            return e % 2 == 0
+        });
+        console.log(result)
+        console.log("Hello World")
+    });
+    // if no return no need for curly braces
+    it.only('without return', () => {
+        var r = [2, 4, 6, 8].every((e) => (e % 2 == 0));
+        console.log(r)
+    });
+
+})

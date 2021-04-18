191d90cf191fe769e8803abfed66d08bed732b81
asus <asu9421@gmail.com>
2021-04-18 06:06:29 +0530

    refactored

---
+++ hooks.js
@@ -0,0 +1,24 @@
+console.log("0");
+describe('level A', function () {
+    console.log("1");
+    beforeEach(function () {
+        console.log("5");
+    });
+
+    describe('level B', function () {
+        console.log("2");
+
+        describe('level C', function () {
+            console.log("3");
+
+            beforeEach(function () {
+                console.log("6");
+            });
+
+            it('foo', function () {
+                console.log("7");
+            });
+        });
+    });
+});
+console.log("4");
